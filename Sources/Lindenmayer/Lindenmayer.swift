9e75930efa3343b1b36b2710fde79cbc69f0d8ef
Joe Heck <heckj@mac.com>
2022-01-18 13:59:30 -0800

    fixing the static factory function on Lindenmayer to be public

--- Sources/Lindenmayer/Lindenmayer.swift
+++ Sources/Lindenmayer/Lindenmayer.swift
@@ -7,18 +7,19 @@
 
 import Foundation
 
+/// A type that provides convenient initializers for L-systems.
 public enum Lindenmayer {
     /// Creates a new Lindenmayer system from an initial state.
     /// - Parameters:
     ///   - axiom: An initial module that represents the initial state of the Lindenmayer system..
-    static func basic(_ axiom: Module) -> LSystemBasic {
+    public static func basic(_ axiom: Module) -> LSystemBasic {
         return LSystemBasic([axiom], state: nil, newStateIndicators: nil)
     }
 
     /// Creates a new Lindenmayer system from an initial state.
     /// - Parameters:
     ///   - axiom: A sequence of modules that represents the initial state of the Lindenmayer system..
-    static func basic(_ axiom: [Module]) -> LSystemBasic {
+    public static func basic(_ axiom: [Module]) -> LSystemBasic {
         return LSystemBasic(axiom, state: nil, newStateIndicators: nil)
     }
 
@@ -26,7 +27,7 @@ public enum Lindenmayer {
     /// - Parameters:
     ///   - axiom: An initial module that represents the initial state of the Lindenmayer system..
     ///   - prng: A psuedo-random number generator to use for randomness in rule productions.
-    static func withRNG<RNGType>(_ axiom: Module, prng: RNGType) -> LSystemRNG<RNGType> {
+    public static func withRNG<RNGType>(_ axiom: Module, prng: RNGType) -> LSystemRNG<RNGType> {
         return LSystemRNG(axiom: [axiom], state: nil, newStateIndicators: nil, prng: RNGWrapper(prng))
     }
 
@@ -34,7 +35,7 @@ public enum Lindenmayer {
     /// - Parameters:
     ///   - axiom: A sequence of modules that represents the initial state of the Lindenmayer system..
     ///   - prng: A psuedo-random number generator to use for for randomness in rule productions.
-    static func withRNG<RNGType>(_ axiom: [Module], prng: RNGType) -> LSystemRNG<RNGType> {
+    public static func withRNG<RNGType>(_ axiom: [Module], prng: RNGType) -> LSystemRNG<RNGType> {
         return LSystemRNG(axiom: axiom, state: nil, newStateIndicators: nil, prng: RNGWrapper(prng))
     }
 
@@ -43,7 +44,7 @@ public enum Lindenmayer {
     ///   - axiom: An initial module that represents the initial state of the Lindenmayer system.
     ///   - prng: A psuedo-random number generator to use for for randomness in rule productions.
     ///   - parameters: An instance of type you provide that the L-system provides to the rules you create for use as parameters.
-    static func withDefines<PType, RNGType>(_ axiom: Module, prng: RNGType, parameters: PType) -> LSystemDefinesRNG<PType, RNGType> {
+    public static func withDefines<PType, RNGType>(_ axiom: Module, prng: RNGType, parameters: PType) -> LSystemDefinesRNG<PType, RNGType> {
         return LSystemDefinesRNG(axiom: [axiom], state: nil, newStateIndicators: nil, parameters: ParametersWrapper(parameters), prng: RNGWrapper(prng), rules: [])
     }
 
@@ -52,7 +53,7 @@ public enum Lindenmayer {
     ///   - axiom: A sequence of modules that represents the initial state of the Lindenmayer system..
     ///   - prng: A psuedo-random number generator to use for for randomness in rule productions.
     ///   - parameters: An instance of type you provide that the L-system provides to the rules you create for use as parameters.
-    static func withDefines<PType, RNGType>(_ axiom: [Module], prng: RNGType, parameters: PType) -> LSystemDefinesRNG<PType, RNGType> {
+    public static func withDefines<PType, RNGType>(_ axiom: [Module], prng: RNGType, parameters: PType) -> LSystemDefinesRNG<PType, RNGType> {
         return LSystemDefinesRNG(axiom: axiom, state: nil, newStateIndicators: nil, parameters: ParametersWrapper(parameters), prng: RNGWrapper(prng), rules: [])
     }
 }
