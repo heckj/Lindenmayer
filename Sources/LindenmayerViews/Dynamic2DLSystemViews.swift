06d335ccf990a9c2f3b23a79bd067733735f5702
Joe Heck <heckj@mac.com>
2022-01-23 14:14:42 -0800

    fixing public initializer

--- Sources/LindenmayerViews/Dynamic2DLSystemViews.swift
+++ Sources/LindenmayerViews/Dynamic2DLSystemViews.swift
@@ -8,39 +8,40 @@
 import Lindenmayer
 import SwiftUI
 
-
-public enum TwoDExamples: String, CaseIterable, Identifiable {
-    case algae
-    case sierpinskiTriangle
-    case kochCurve
-    case dragonCurve
-    case fractalTree
-    case barnsleyFern
-    public var id: String { rawValue }
-    /// The example seed L-system
-    public var lsystem: LindenmayerSystem {
-        switch self {
-        case .algae:
-            return Examples2D.algae
-        case .sierpinskiTriangle:
-            return Examples2D.sierpinskiTriangle
-        case .kochCurve:
-            return Examples2D.kochCurve
-        case .dragonCurve:
-            return Examples2D.dragonCurve
-        case .fractalTree:
-            return Examples2D.fractalTree
-        case .barnsleyFern:
-            return Examples2D.barnsleyFern
-        }
-    }
-}
-
 /// A view that allows you to choose from a collection of the built-in 2D L-systems and display the 2D representation of the L-system at the number of iterations that you select in the view.
 @available(macOS 12.0, iOS 15.0, *)
 public struct Dynamic2DLSystemViews: View {
+
+    enum TwoDExamples: String, CaseIterable, Identifiable {
+        case algae
+        case sierpinskiTriangle
+        case kochCurve
+        case dragonCurve
+        case fractalTree
+        case barnsleyFern
+        public var id: String { rawValue }
+        /// The example seed L-system
+        public var lsystem: LindenmayerSystem {
+            switch self {
+            case .algae:
+                return Examples2D.algae
+            case .sierpinskiTriangle:
+                return Examples2D.sierpinskiTriangle
+            case .kochCurve:
+                return Examples2D.kochCurve
+            case .dragonCurve:
+                return Examples2D.dragonCurve
+            case .fractalTree:
+                return Examples2D.fractalTree
+            case .barnsleyFern:
+                return Examples2D.barnsleyFern
+            }
+        }
+    }
+    
     @State private var evolutions: Double = 0
     @State private var selectedSystem = TwoDExamples.fractalTree
+    
     public var body: some View {
         VStack {
             Picker("L-System", selection: $selectedSystem) {
@@ -64,8 +65,11 @@ public struct Dynamic2DLSystemViews: View {
             .padding()
             Lsystem2DView(system: selectedSystem.lsystem.evolved(iterations: Int(evolutions)),
                           displayMetrics: false)
+                .padding()
         }
     }
+    
+    public init() {}
 }
 
 @available(macOS 12.0, iOS 15.0, *)
